98c169f1aa05ae42eae86d570d313f9894cb8b15
andrehhh <andrehanjaya9@gmail.com>
2021-06-08 20:42:02 +0800

    Accordion functionalities done

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,19 @@
 import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
+
 function App() {
-  return <h2>accordion project setup</h2>;
+  return (
+    <div style={{color: 'white'}}>
+      {
+        data.map((question) => {
+          return (
+            <SingleQuestion key={question.id} question={question} />
+          )
+        })
+      }
+    </div>
+  )
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,26 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+
+const Question = ({ question }) => {
+  const { title, info } = question;
+
+  // showInfo controls whether the info is shown or not.
+  const [showInfo, setshowInfo] = useState(false); 
+
+  // On Button click will toggle the showInfo to true/false.
+  // This causes the conditional rendering to function and renders the info if showInfo is true.
+  // The text inside the button also changes using ternary operator.
+  return (
+    <>
+      <h3>{title}</h3>
+      <button
+        style={{padding: '0.25rem 0.5rem'}}
+        onClick={() => setshowInfo(!showInfo)}> 
+          { showInfo ? '-' : '+' }
+      </button>
+      {showInfo && info}
+    </>
+  )
 };
 
 export default Question;
