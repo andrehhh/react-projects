4a5675e113291f1e4abf74ecace6a89d10b0754c
andrehhh <andrehanjaya9@gmail.com>
2021-06-08 17:28:01 +0800

    Added more functionalities and comments

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -10,7 +10,10 @@ function App() {
   const [tours, setTours] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
+  // Function that fetches all tours from the url
+  const fetchTours = () => {
+    setIsLoading(true); // Before it starts fetching, set isLoading true.
+
     fetch(url)
     .then((response) => {
       if(response.status >= 200 && response.status <= 299) {
@@ -25,15 +28,30 @@ function App() {
       setIsLoading(false);
     })
     .catch((error) => console.log(error));
+  }
+
+  useEffect(() => {
+    fetchTours();
 
     return () => {
       // cleanup
     }
   }, [])
 
+  // removeTour takes in ID and filters it out from the current tours state
   const removeTour = (id) => {
     const newTours = tours.filter((tour) => tour.id !== id);
     setTours(newTours);
+  }
+
+  // If the fetch has finished and tour length is 0, return No Tours Left.
+  if(isLoading === false && tours.length === 0) {
+    return (
+      <>
+        <h1>No Tours Left</h1>
+        <button onClick={fetchTours}>Refresh</button>
+      </>
+    )
   }
 
   return (
