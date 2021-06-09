19e26ad4d505fafc63e65793d86f0b1d035df155
andrehhh <andrehanjaya9@gmail.com>
2021-06-10 02:22:31 +0800

    Colour Generator functionalities done

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -4,7 +4,45 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+
+  // Color list
+  const [colors, setColors] = useState(new Values('#45294d').all(10));
+
+  // Color value on input text
+  const [color, setColor] = useState('#45294d');
+
+  // On submit button, updates color text
+  const generateColors = (e) => {
+    e.preventDefault();
+    setColors(new Values(color).all(10));
+  }
+
+  // On input change, set to the new color value. 
+  // If value is empty, set as '#'. This will act as a prefix for the hex value.
+  const colorOnChange = (value) => {
+    if(value === "") {
+      setColor("#");
+    } else {
+      setColor(value);
+    }
+  }
+
+  return (
+    <>
+      <form onSubmit={generateColors}>
+        <label htmlFor="inputColour"><h2>Color Generator</h2></label>
+        <input type="text" value={color} onChange={(e) => colorOnChange(e.target.value)}/>
+        <button type="submit">Generate</button>
+      </form>
+      {
+        colors.map((color, index) => {
+          return (
+            <SingleColor key={index} color={`#${color.hex}`} weight={color.weight} />
+          )
+        })
+      }
+    </>
+  )
 }
 
 export default App
--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,25 @@
 import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
-}
+const SingleColor = React.memo(({ color, weight }) => {
+
+  const [showAlert, setShowAlert] = useState(false);
+
+  // Copy to clipboard function, and display alert which times out after 2 seconds.
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(color);
+    setShowAlert(true);
+    setTimeout(() => {
+      setShowAlert(false)
+    }, 2000);
+  }
+
+  return (
+    <div style={{backgroundColor: color}} onClick={copyToClipboard}>
+      <h1>Color</h1>
+      <p>{weight}%</p>
+      <p>{showAlert && 'Copied to Clipboard'}</p>
+    </div>
+  )
+})
 
 export default SingleColor
